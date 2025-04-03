f8900a3fe1c021e97ba4c7e522d5fe3159a11306
kumarianj <anjaliku7947@gmail.com>
2025-04-03 15:39:31 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
+import Pipeline from './Pipeline';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={{color:'red'}}>
       This is jenkins pipeline  
+      <Pipeline/>
     </div>
   );
 }
---
+++ src/Pipeline.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Pipeline = () => {
+  return (
+    <div style={{  justifyContent:'center ', color:'green'}}> connection with git to jenkins in pipeline</div>
+  )
+}
+
+export default Pipeline
