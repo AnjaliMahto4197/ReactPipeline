7bb20452e405730aba324d537741b819367d6836
kumarianj <anjaliku7947@gmail.com>
2025-04-04 14:22:10 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,1 +1,13 @@
-kiyhb,hkhuydbrcguhshbxasefew
+
+import './App.css';
+import Pipeline from './Pipeline';
+function App() {
+  return (
+    <div className="App">
+      this is pipeline 
+      <Pipeline/>
+    </div>
+  );
+}
+
+export default App;
